5dc4527d401eeacedd92110ab1d617a4bc1f518f
Aeternis12 <lmm1409@yahoo.com>
2025-05-28 03:22:27 -0400

    Add project files.

---
+++ Asana.CLI/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Asana.Library.Model;
+using System;
+
+namespace Asana
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ToDo MyFirstToDo = new ToDo();
+            Console.WriteLine(MyFirstToDo.Name);
+        }
+    }
+}
