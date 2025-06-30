00d0a2f10d8a629b37f5df877954fbee01e8c1a3
Aeternis12 <lmm1409@yahoo.com>
2025-06-30 13:38:57 -0400

    Finished implementation Add ToDo feature. Closes # 13

--- Asana.Library/Model/ToDo.cs
+++ Asana.Library/Model/ToDo.cs
@@ -8,12 +8,17 @@ namespace Asana.Library.Model
 {
     public class ToDo
     {
+        public ToDo()
+        {
+            Id = 0;
+            IsCompleted = false;
+        }
+
         public string? Name { get; set; }
         public string? Description { get; set; }
         public int? Priority { get; set; } 
         public bool? IsCompleted { get; set; }
         public int Id { get; set; }
-        public int ProjectId { get; set; }
 
         public override string ToString()
         {
---
+++ Asana.Maui/ViewModels/ToDoDetailViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Asana.Library.Model;
+using Asana.Library.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Maui.ViewModels
+{
+    public class ToDoDetailViewModel
+    {
+        public ToDoDetailViewModel() {
+            Model = new ToDo(); 
+        }
+
+        public ToDo? Model { get; set; }
+
+        public List<int> Priorities
+        {
+            get
+            {
+                return new List<int> { 1, 2, 3, 4 };
+            }
+        }
+
+        public int SelectedPriority
+        {
+            get
+            {
+                return Model?.Priority ?? 4;
+            }
+            set
+            {
+                if(Model != null && Model.Priority != value)
+                {
+                    Model.Priority = value;
+                }
+            }
+        }
+
+        public void AddOrUpdateToDo()
+        {
+            ToDoServiceProxy.Current.AddOrUpdate(Model);
+        }
+
+        
+    }
+}
